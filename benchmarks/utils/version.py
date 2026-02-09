13b8aa7d8ad0c89d6906c0d8449bb57654abc880
Lyuwen Fu <fulvwen@gmail.com>
2026-02-09 10:59:19 +0800

    update software-agent-sdk to modified version but freeze git hash to use the same agent servers

--- benchmarks/utils/version.py
+++ benchmarks/utils/version.py
@@ -24,4 +24,5 @@ def get_sdk_sha() -> str:
 
 
 SDK_SHA = get_sdk_sha()
+SDK_SHA = "f011127916917cb3a3a2c29d2c5b151ef2464f44" # Fix SDK version to freeze agent server images
 SDK_SHORT_SHA = SDK_SHA[:7]
